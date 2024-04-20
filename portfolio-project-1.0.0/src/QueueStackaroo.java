fc41e47f2e1931250df85b10a764bd56fabc60a8
Andrew Turk <144287363+Splornk@users.noreply.github.com>
2024-04-19 23:08:50 -0400

    Add files via upload
    Added all the initial uploads.

---
+++ portfolio-project-1.0.0/src/QueueStackaroo.java
@@ -0,0 +1,90 @@
+/**
+ * QueueStackaroo Enhanced Methods.
+ *
+ * @param <T>
+ *            The type stored in {@code this}
+ */
+public interface QueueStackaroo<T> extends QueueStackarooKernel<T> {
+    /**
+     * Reports the front of this. If this.queueMode switches the data will be
+     * mixed up.
+     *
+     * @aliases reference returned by front
+     * @return the front entry of this
+     * @requires this /= <>
+     * @ensures <front> is prefix of this
+     */
+    T front();
+
+    /**
+     * Attempts to replace the front of this with x, and returns the old front.
+     * Guanteed to work if this is not in queue mode. If this is in queue mode x
+     * may end up somewhere in the middle.
+     *
+     * @param x
+     *            - the new front entry
+     * @aliases reference x
+     * @updates {@code this}
+     * @return the old front entry
+     * @requires this /= <>
+     * @ensures <pre>
+     * <replaceFront> is prefix of #this and
+     * if this is not in queue mode
+     *      this = <x> * #this[1, |#this|)
+     * else
+     *      this is perms(<x> * this[1, |#this|)
+     * </pre>
+     */
+    T replaceFront(T x);
+
+    /**
+     * Concatenates ("appends") entries in q to the end of this if in queueMode,
+     * else concatenates ("appends") entries in q to the top of this.
+     *
+     * @param q
+     *            - the Queue to be appended to the end of this
+     * @updates {@code this}
+     * @clears q
+     * @ensures this is perms(#this * #q, this * q)
+     */
+    void append(QueueStackaroo<T> q);
+
+    /**
+     * Reverses ("flips") {@code this}. If this.queueMode switches the data will
+     * be mixed up.
+     *
+     * @updates {@code this}
+     * @ensures this is perms(rev(#this))
+     */
+    void flip();
+
+    /**
+     * Rotates {@code this}. If this.queueMode switches the data will be mixed
+     * up.
+     *
+     * @param distance
+     *            distance by which to rotate
+     * @updates {@code this}
+     * @ensures <pre>
+     * if #this = <> then
+     *  this = #this
+     * else
+     *  this is perms(#this[distance mod |#this|, |#this|)
+     *  * #this[0, distance mod |#this|))
+     * }
+     * </pre>
+     */
+    void rotate(int distance);
+
+    /**
+     * Switches this.queue_mode, however there is a 1/3 chance that all of your
+     * data is lost.
+     *
+     * @updates this
+     * @ensures <pre>
+     * this.queue_mode = !this.queue_mode but
+     * 1/3 chance this.length = |{}|
+     * </pre>
+     */
+    void gamble();
+}
---
+++ portfolio-project-1.0.0/test/QueueStackarooKernelTest.java
@@ -0,0 +1,126 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Testing the kernel methods of the QueueStackaroo component.
+ */
+public class QueueStackarooKernelTest {
+
+    /**
+     * Testing the constructor.
+     */
+    @Test
+    public void constructorTest() {
+        QueueStackaroo<Integer> q = new QueueStackaroo1<Integer>();
+        assertEquals(true, q.isInQueueMode());
+        assertEquals(0, q.length());
+    }
+
+    /**
+     * Testing add in queue mode.
+     */
+    @Test
+    public void addInQueue() {
+        QueueStackaroo<Integer> q = new QueueStackaroo1<Integer>();
+        QueueStackaroo<Integer> qCopy = new QueueStackaroo1<Integer>();
+        q.add(1);
+        q.add(2);
+        int front = q.remove();
+        qCopy.add(1);
+        assertEquals(1, front);
+        assertEquals(qCopy, q);
+    }
+
+    /**
+     * Testing add in stack mode.
+     */
+    @Test
+    public void addInStack() {
+        QueueStackaroo<Integer> q = new QueueStackaroo1<Integer>();
+        QueueStackaroo<Integer> qCopy = new QueueStackaroo1<Integer>();
+
+        while (q.isInQueueMode()) {
+            q.add(1);
+            q.remove();
+        }
+        while (qCopy.isInQueueMode()) {
+            qCopy.add(1);
+            qCopy.remove();
+        }
+
+        q.add(1);
+        q.add(2);
+        int front = q.remove();
+        qCopy.add(1);
+        qCopy.add(2);
+        qCopy.remove();
+        assertEquals(2, front);
+        assertEquals(qCopy, q);
+    }
+
+    /**
+     * Testing remove.
+     */
+    @Test
+    public void removeInQueue() {
+        QueueStackaroo<Integer> q = new QueueStackaroo1<Integer>();
+        QueueStackaroo<Integer> qCopy = new QueueStackaroo1<Integer>();
+        q.add(1);
+        q.add(2);
+        int front1 = q.remove();
+        int front2 = q.remove();
+        assertEquals(1, front1);
+        assertEquals(2, front2);
+        assertEquals(qCopy, q);
+    }
+
+    /**
+     * Testing length when empty.
+     */
+    @Test
+    public void emptyLengthTest() {
+        QueueStackaroo<Integer> q = new QueueStackaroo1<Integer>();
+        QueueStackaroo<Integer> qCopy = new QueueStackaroo1<Integer>();
+        q.add(1);
+        q.add(2);
+        qCopy.add(1);
+        qCopy.add(2);
+        assertEquals(2, q.length());
+        assertEquals(qCopy, q);
+    }
+
+    /**
+     * Testing length when non-empty.
+     */
+    @Test
+    public void nonEmptyLengthTest() {
+        QueueStackaroo<Integer> q = new QueueStackaroo1<Integer>();
+        QueueStackaroo<Integer> qCopy = new QueueStackaroo1<Integer>();
+        assertEquals(0, q.length());
+        assertEquals(qCopy, q);
+    }
+
+    /**
+     * Testing isInQueueMode.
+     */
+    @Test
+    public void isInQueueModeTest() {
+        QueueStackaroo<Integer> q = new QueueStackaroo1<Integer>();
+        QueueStackaroo<Integer> qCopy = new QueueStackaroo1<Integer>();
+
+        while (q.isInQueueMode()) {
+            q.add(1);
+            q.remove();
+            if (!q.isInQueueMode()) {
+                System.out.println("Has Switched");
+            }
+        }
+        while (qCopy.isInQueueMode()) {
+            qCopy.add(1);
+            qCopy.remove();
+        }
+
+        assertEquals(qCopy, q);
+    }
+}
